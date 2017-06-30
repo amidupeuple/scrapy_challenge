441aea4ff7636361dd959a6f4fdc3a0578f5d9a2
amidupeuple <danyapivovarov@gmail.com>
2017-06-30 17:30:00 +0300

    scrapper is ready. training data was scrapped. on a way of data preparing for model.

--- spider.py
+++ spider.py
@@ -1,21 +1,50 @@
 # -*- coding: utf-8 -*-
 
 import scrapy
+import re
 
 class CommentsSpider(scrapy.Spider):
-    name = "comments"
+    name = "spider_dns_shop"
+    
+    #this is url which will be concatenated with particular brand
+    brand_url = "http://www.dns-shop.ru/catalog/17a8a01d16404e77/smartfony/?brand="
+    
     start_urls = [
-        'http://www.dns-shop.ru/product/5d4994a37de23361/35-smartfon-dexp-ixion-es135-hit-4-gb-belyj/opinion/',
-		'http://www.dns-shop.ru/product/2b2e199946d83361/35-smartfon-highscreen-pure-j-512-mb-cernyj/opinion/'
+        'http://www.dns-shop.ru/catalog/17a8a01d16404e77/smartfony/'
     ]
 
+    
     def parse(self, response):
-        for opinion in response.xpath('//div[@class="opinion-item"]'):
-            yield {
-                'rating': opinion.xpath(".//div[@class='product-item-rating rating']/@data-rating").extract_first(),
-                'comment': opinion.xpath('.//li[./span/text()="Комментарий"]/span[@class="description-text"]/.').extract_first()
-            }
-
-        '''next_page = response.css('li.next a::attr("href")').extract_first()
-        if next_page is not None:
-            yield response.follow(next_page, self.parse)'''
+        print("@@@: response url: " + response.url)
+        
+        if "http://www.dns-shop.ru/catalog/17a8a01d16404e77/smartfony/" == response.url:
+            #This is initial if - we will get here only at first time when origin response will be returned
+            #The goal - prepare all urls for available brands
+            brand_div = response.xpath("//div[@data-id='brand']")
+            brands_list = brand_div.xpath(".//div[@data-role='filter-block-item']/input[@type='checkbox']/@data-id").extract()
+            for b in brands_list:
+                b_url = self.brand_url + b
+                yield response.follow(b_url, self.parse)
+        elif "brand" in response.url:
+            #Here we go when response with particular brand is returned
+            #Goal - prepare urls for all products
+            for item in response.xpath("//a[@class='show-popover ec-price-item-link image-container']/@href").extract():
+                if item is not None:
+                    item_url = re.sub("\?(.*)", "opinion/", item)
+                    print("@@@: product url: " + item_url)
+                    yield response.follow(item_url, self.parse)
+        elif "product" in response.url:
+            for opinion in response.xpath('//div[@class="opinion-item"]'):
+                if opinion:
+                    yield {
+                        'rating': opinion.xpath(".//div[@class='product-item-rating rating']/@data-rating").extract_first(),
+                        'comment': opinion.xpath('.//li[./span/text()="Комментарий"]/span[@class="description-text"]/.').extract_first()
+                    }
+            
+                next_list = response.xpath("//li[@class='next']")
+                if len(next_list) > 0:
+                    next = next_list[0]
+                    next_url = next.xpath(".//a/@href").extract_first()
+                    if next_url is not None:
+                        print("@@@: comment url: " + next_url)
+                        yield response.follow(next_url, self.parse)
--- spider_utils.py
+++ spider_utils.py
@@ -1,6 +1,7 @@
 # -*- coding: utf-8 -*-
 import codecs
 import re
+import os
 
 def remove_hml_tags(file_name):
     f = codecs.open(file_name, "r", "utf-8")
@@ -8,5 +9,16 @@ def remove_hml_tags(file_name):
     
     f = codecs.open(file_name, "w", "utf-8")
     replaced = re.sub("<[^>]*>", "", data)
+    
+    replaced = remove_empry_lines(replaced)
+    
     f.write(replaced)
     f.close()
+
+    
+def remove_empry_lines(text):
+    return os.linesep.join([s for s in text.splitlines() if s])
+        
+        
+def substitude_parameters_with_opinion(origin_url):
+    return re.sub("\?(.*)", "opinion/", origin_url)
